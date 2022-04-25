b3d4faac42d6342961a55695f17039f6bb31869d
Babayev Andriy <babayevandriy@gmail.com>
2022-04-25 21:18:06 +0300

    firstday

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// // добавления слайдера slick на сайт
+$(function(){
+    $('.rev_slider').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });})
