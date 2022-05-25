99e98dca80f2f5af3386e0bf11676b32ee786f41
Aндрей Бабаев <andriyko1979@gmail.com>
2022-05-25 17:20:49 +0300

    add_ten_finaly

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 window.onload = function () {
   showSlides(slideIndex);
   filterSelection('all');
+};
+window.onclick = function (event) {
+  let modal = document.querySelector('.modal');
+  let span = document.getElementsByClassName('close');
+  let bigImage = document.querySelector('.modal-content');
+  console.log(modal);
 
+  if (event.target.className === 'test1') {
+    console.log(event.target.src);
+    modal.style.display = 'block';
+    bigImage.setAttribute('src', event.target.src);
+    console.log(event.target.className === 'test1');
+  }
+  if (event.target.className === 'close') {
+    modal.style.display = 'none';
+  }
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
   let filterList = document.getElementById('filter-list');
   let btns = filterList.getElementsByClassName('btn');
 
@@ -14,23 +32,17 @@ window.onload = function () {
   }
 };
 
-// Почитай про команду window.onload !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// Для dots та btn потрібні різні класни active, вони разом працювали для slider та buttons!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 let slideIndex = 1;
 
 function plusSlide() {
   showSlides((slideIndex += 1));
 }
-
 function minusSlide() {
   showSlides((slideIndex -= 1));
 }
-
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName('item');
@@ -82,20 +94,6 @@ function imgRemoveClass(element, name) {
   }
   element.className = arr1.join(' ');
 }
-// function showImage(event) {
-//   let modal = document.getElementById('myModal');
-//   let img = document.getElementsById('myImg');
-//   let modalImg = document.getElementById('img01');
-
-//   img.onclick = function () {
-//     modal.style.display = 'block';
-//     modalImg.src = this.src;
-//   };
-//   let span = document.getElementsByClassName('close')[0];
-//   span.onclick = function () {
-//     modal.style.display = 'none';
-//   };
-// }
 document.addEventListener('DOMContentLoaded', function () {
   let modal = document.querySelector('myImg');
   let img = new bootstrap.Modal(document.querySelector('img01'));
